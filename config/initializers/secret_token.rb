f87f1fa18091405a34707566b830c01a69fb0653
sashasoft <kovalenko.alexandrs@gmail.com>
2014-03-29 02:08:28 +0200

    DZ12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrudForPost::Application.config.secret_key_base = '46ff0b3f1769250a6da14e6713276fee28752f07417d03b9a9b487c82bc0532e3f3f703fc3d28c06ea6fa514749ff6d5b131138e3a0c2787e3e7ea031bb701e8'
